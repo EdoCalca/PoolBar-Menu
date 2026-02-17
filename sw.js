208645189267f8c38177ca87cfadd5134f566613
EdoCalca <edocalca3005@gmail.com>
2026-02-17 12:07:53 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "poolbar-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./menu.pdf",
+  "./cocktail.pdf"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
